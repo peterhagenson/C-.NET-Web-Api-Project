d612a61e71ea970a48fbbbc334d34a6dba9cc74e
Peter Hagenson <p.hagenson@gmail.com>
2022-09-21 08:44:58 -0500

    routes working in postman

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -33,6 +33,8 @@ public ActionResult<Pizza> Get(int id)
 [HttpPost]
 public IActionResult Create(Pizza pizza)
 {
+    PizzaService.Add(pizza);
+    return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
     //this code will save the pizza and return a result
 }
 
@@ -40,6 +42,16 @@ public IActionResult Create(Pizza pizza)
 [HttpPut("{id}")]
 public IActionResult Update(int id, Pizza pizza)
 {
+    if (id != pizza.Id)
+        return BadRequest();
+           
+    var existingPizza = PizzaService.Get(id);
+    if(existingPizza is null)
+        return NotFound();
+   
+    PizzaService.Update(pizza);           
+   
+    return NoContent();
     // this code will update the pizza and return a result
 }
 
@@ -47,6 +59,14 @@ public IActionResult Update(int id, Pizza pizza)
 [HttpDelete("{id}")]
 public IActionResult Delete(int id)
 {
+    var pizza = PizzaService.Get(id);
+   
+    if (pizza is null)
+        return NotFound();
+       
+    PizzaService.Delete(id);
+   
+    return NoContent();
     //this code will delete the pizza and return a result
 }
 
